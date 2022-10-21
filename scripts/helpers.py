b75b2fabbafa576f863c5cc3c95e3eb0e96ccf81
nahomHmichael <nahomhabtemichael@gmail.com>
2022-10-21 16:03:23 +0300

    Update

--- scripts/helpers.py
+++ scripts/helpers.py
@@ -4,8 +4,31 @@ import seaborn as sns
 import scipy.stats as stats
 import logging
 import numpy as np
+import math as m
 
 class Helper:
+    """
+    summary: helper class for EDA
+    """
     def __init__(self) -> None:
         pass
+    
+    def get_df_info(self, df):
+        
+        return (df.describe().T.style.bar(subset=['mean'], color='#205ff2').background_gradient(subset=['std'],
+                                                                                                cmap='Reds').background_gradient(subset=['50%'], cmap='coolwarm'))
+  
+    def percent_missing(self,df):
+
+        total_cells = m.prod(df.shape)
+        missing_count = df.isnull().sum()
+        total_missing = missing_count.sum()
+        print("Total Percentage of Missing values: " +
+                f"{round(((total_missing/total_cells)*100))}" +"% ")
+
+  
+  
+    # def check_outlier(self, df, )
+    
+    
     
