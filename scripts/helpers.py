edbd006d133d92a85e6d5add0d20828da9e24128
nahomHmichael <nahomhabtemichael@gmail.com>
2022-10-25 00:28:43 +0300

    update

--- scripts/helpers.py
+++ scripts/helpers.py
@@ -7,6 +7,8 @@ import numpy as np
 import math as m
 import holidays
 from datetime import datetime
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import Normalizer
 class Helper:
     """
     summary: helper class for EDA
@@ -45,6 +47,40 @@ class Helper:
             return 0
         else:  
             return 1
+        
+    def show_corr(self, df, title, size=[17,10], range=None):
+ 
+        try:
+            # correlation matrix
+            if range is None:
+                corr_matrix = df.corr()
+            else:
+                if(range[1] == -10):
+                    corr_matrix = df.iloc[:,range[0]:].corr()
+                else:
+                    corr_matrix = df.iloc[:,range[0]:range[1]].corr()
+            matrix = np.triu(corr_matrix)
+            fig, ax = plt.subplots(figsize=(size[0], size[1]))
+            plt.title(title)
+            ax = sns.heatmap(corr_matrix, annot=True, mask=matrix)
+        except:
+                print('not successful')
+                
+    def label_encoder(self,df,columns=None):
+        if columns == None:
+            columns = df.select_dtypes(exclude = ['number'])
+        le = LabelEncoder()
 
+        for col in columns:
+            df[col] = le.fit_transform(df[col])
+
+        return df
+    def normalizer(self,df,columns):
     
-    
+        nrm = Normalizer()
+        df_norm = df.copy(deep=True)
+        df_norm[columns] = pd.DataFrame(nrm.fit_transform( df_norm[columns]), columns=columns)
+        return df_norm
+
+        
+        
