4e06220f6072f9ad4147d1cd599695814508542e
nahomHmichael <nahomhabtemichael@gmail.com>
2022-10-24 18:02:47 +0300

    added additional script

--- scripts/helpers.py
+++ scripts/helpers.py
@@ -5,7 +5,8 @@ import scipy.stats as stats
 import logging
 import numpy as np
 import math as m
-
+import holidays
+from datetime import datetime
 class Helper:
     """
     summary: helper class for EDA
@@ -17,6 +18,8 @@ class Helper:
         
         return (df.describe().T.style.bar(subset=['mean'], color='#205ff2').background_gradient(subset=['std'],
                                                                                                 cmap='Reds').background_gradient(subset=['50%'], cmap='coolwarm'))
+        
+    
   
     def percent_missing(self, df):
 
@@ -26,6 +29,15 @@ class Helper:
         print("Total Percentage of Missing values: " +
                 f"{round(((total_missing/total_cells)*100))}" +"% ")
         
+    def is_holiday(self,Date):
+        ng_holidays = holidays.country_holidays('NG')
+        try:
+            dt = datetime.strptime(Date, '%Y-%m-%d %H:%M:%S').date()
+            if dt in ng_holidays:
+                return 1
+            else: return 0
+        except Exception as e:
+             return 0
 
     
     
